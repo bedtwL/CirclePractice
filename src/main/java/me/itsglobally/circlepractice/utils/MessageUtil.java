1b52098c3725a93f2457c9c5edaebad3d10dcf10
ItsGlobally <itsglobally1324@gmail.com>
2025-08-29 12:16:17 +0800

    .

--- src/main/java/me/itsglobally/circlepractice/utils/MessageUtil.java
+++ src/main/java/me/itsglobally/circlepractice/utils/MessageUtil.java
@@ -2,6 +2,7 @@ package me.itsglobally.circlePractice.utils;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import me.itsglobally.circlePractice.CirclePractice;
@@ -15,7 +16,7 @@ public class MessageUtil {
     }
 
     public static void sendActionBar(Player player, String message) {
-        Component component = MINI.deserialize(ChatColor.translateAlternateColorCodes('&', message));
+        Component component = LegacyComponentSerializer.legacyAmpersand().deserialize(message);
         CirclePractice.audience(player).sendActionBar(component);
     }
     public static void sendTitle(Player player, String title, String subtitle) {
